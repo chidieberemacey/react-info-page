e788579ccd479971c663218ef4c66ea34758b4fd
Chidiebere Macey <egbomaceyskyla@gmail.com>
2023-08-30 18:36:49 +0100

    "first edit"

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <img src="../images/logo.png" alt="solo"/>
+            <h3>React Facts</h3>
+            <h4>React Course - Project 1</h4>
+        </div>
+    )
+}
